762b762d94ab434c1a2af9a3dea338af10bee0f9
carlos <dc.unicomtec@gmail.com>
2025-05-14 10:52:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+VANTA.CELLS({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  color1: 0x5c31e8,
+  color2: 0x2c2ce3,
+  speed: 2.00
+})
+
+new Typed('#typed-output', {
+  strings: [
+      "I am an aspiring app developer.",
+      "I love creating responsive website.",
+      "Passionate about design and coding."
+  ],
+  typeSpeed: 50,       // Speed of typing
+  backSpeed: 25,       // Speed of backspacing
+  backDelay: 1000,     // Delay before backspacing
+  loop: true           // Repeat the typing loop
+});
+

00cd582eedc285690c4172ed90f9f3eaed8e6548
Dennis Carlos <dc.unicomtec@gmail.com>
2025-05-14 14:09:34 +0800

    indented codes by 2 spaces

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ VANTA.CELLS({
   color1: 0x5c31e8,
   color2: 0x2c2ce3,
   speed: 2.00
-})
+});
 
 new Typed('#typed-output', {
   strings: [
-      "I am an aspiring app developer.",
-      "I love creating responsive website.",
-      "Passionate about design and coding."
+    "I am an aspiring app developer.",
+    "I love creating responsive website.",
+    "Passionate about design and coding."
   ],
   typeSpeed: 50,       // Speed of typing
   backSpeed: 25,       // Speed of backspacing
